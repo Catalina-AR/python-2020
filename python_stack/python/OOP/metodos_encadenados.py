fddab4c82ddd1c8f33007638159a72b9b99e8a38
Catalina <CATALINA.ACUNAR@correoaiep.cl>
2020-12-29 22:49:11 -0300

    se sube tarea metodos encadenados oop

--- python_stack/python/OOP/metodos_encadenados.py
+++ python_stack/python/OOP/metodos_encadenados.py
@@ -17,16 +17,11 @@ class User:
         self.amount_balance -= amount
         other_user.make_deposits(amount)
 
-    class User:
-        def make_deposit(self, amount):
-            catalina.make_deposit(50).make_deposit(50).make_deposit(50).make_withdrawal(10).display_user_balance()
-            juan.make_deposit(30).make_deposit(30).make_withdrawal(10).make_withdrawal(10).display_user_balance()
-            marco.make_deposit(100).make_withdrawal(10).make_withdrawal(10).make_withdrawal(10).display_user_balance()
-            return self
-
 catalina = User("Catalina", "catalina@gmai.com")
 juan = User("Juan", "juan@gmail.com")
 marco = User("Marco", "marco@gmail.com.com")
 
-
+catalina.make_deposit(50).make_deposit(50).make_deposit(50).make_withdrawal(10).display_user_balance()
+juan.make_deposit(30).make_deposit(30).make_withdrawal(10).make_withdrawal(10).display_user_balance()
+marco.make_deposit(100).make_withdrawal(10).make_withdrawal(10).make_withdrawal(10).display_user_balance()
 
