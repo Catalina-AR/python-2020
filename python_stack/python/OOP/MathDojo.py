163c2da4c5d11dfb3ae10e1f811c7fc10bf3093b
Catalina <CATALINA.ACUNAR@correoaiep.cl>
2021-01-07 21:57:59 -0300

    se sube tarea  mathdojo python oop

--- python_stack/python/OOP/MathDojo.py
+++ python_stack/python/OOP/MathDojo.py
@@ -1,18 +1,31 @@
-class MathDojo(object):
-     def __init__(self):
-         self.resultado = 0
-     def suma(self, *x):
-         self.sumale = 0
-         for value in x:
-             self.sumale += value
-         self.resultado += self.sumale
-         return self
-     def resta(self, *y):
-         self.restale = 0
-         for value in y:
-             self.restale += value
-         self.resultado -= self.restale
-         return self
+class MathDojo:
+    def __init__(self):
+    	self.result = 0
 
-md = MathDojo().suma(2).suma(2,5,1).resta(3,2).resultado
-print(md)
+    def add(self, num, *nums):
+        self.result += num
+        for a in nums:
+            self.result += a
+        return self
+
+    def subtract(self, num, *nums):
+        self.result -= num
+        for a in nums:
+            self.result -= a
+        return self
+
+md1 = MathDojo()
+md2 = MathDojo()
+md3 = MathDojo()
+md4 = MathDojo()
+
+x1 = md1.add(2).add(2, 5, 1).subtract(3, 2).result
+print(x1)
+x2 = md2.add(330).subtract(15,15).add(30).subtract(10).result
+print(x2)
+x3 = md3.add(500,100,100).subtract(100, 100).result
+print(x3)
+x4 = md4.add(1000,300).subtract(500,500).result
+print(x4)
+
+
