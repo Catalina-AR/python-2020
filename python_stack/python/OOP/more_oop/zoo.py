a0206aa69276467008409bad06e5b85f89ed8e62
Catalina <CATALINA.ACUNAR@correoaiep.cl>
2021-01-12 18:27:41 -0300

    se sube tarea  zoo python oop

--- python_stack/python/OOP/more_oop/zoo.py
+++ python_stack/python/OOP/more_oop/zoo.py
@@ -1,60 +1,68 @@
-class animal(object):
-    def __init__(self, nombre: str, edad, salud: int, felicidad: int):
-        self.nombre = nombre
-        self.edad = edad
-        self.salud = salud
-        self.felicidad = felicidad
-        self.especie = str()
-
-    def display_info(self):
-        s = f"This {self.especie} is named {self.nombre}, is {self.salud}, and is {self.felicidad}."
-        return s
-
-    def alimentar(self):
-        self.salud += 10
-        self.felicidad += 10
-
-
-class leon(animal):
-    def __init__(self, nombre: str, edad="5", salud: int = 3, felicidad: int = 3, macho: bool = True):
-        super().__init__(nombre, edad, salud, felicidad)
-        self.macho = macho
-
-
-class oso(animal):
-    def __init__(self, nombre: str, edad="unknown", salud: int = 2, felicidad: int = 10, color: str="gris"):
-        super().__init__(nombre, edad, salud, felicidad)
-        self.color = color
-
-
-class tigre(animal):
-    def __init__(self, nombre: str, edad="desconocida", salud: int = 4, felicidad: int = 5):
-        super().__init__(nombre, edad, salud, felicidad)
-
-
 class Zoo:
-    def __init__(self, zoologico):
-        self.animal = []
-        self.nombre = zoologico
+    def __init__(self, zoo_name):
+        self.animals = []
+        self.name = zoo_name
 
-    def add_leon(self, nombre):
-        self.animal.append(leon(nombre))
-
-    def add_oso(self,nombre):
-        self.animal.append(oso(nombre))
-
-    def add_tigre(self, nombre):
-        self.animal.append(tigre(nombre))
+    def add_animal(self, name, edad, tipo):
+        if str(tipo)=='leon':
+            self.animals.append(Leones(name, edad))
+        elif str(tipo)=='tigre':
+            self.animals.append(Tigres(name, edad))
+        elif str(tipo) == 'oso':
+            self.animals.append(Osos(name, edad))
+        else:
+            print('tipo no implementado')
+        return self.animals[len(self.animals)-1]
 
     def print_all_info(self):
-        print("-" * 30, self.nombre, "-" * 30)
-        for animal in self.animal:
+        print("-"*30, self.name, "-"*30)
+        for animal in self.animals:
             animal.display_info()
-zoo1 = Zoo("John's Zoo")
-zoo1.add_leon("Nala")
-zoo1.add_leon("Simba")
-zoo1.add_oso("Lolo")
-zoo1.add_oso("Kira")
-zoo1.add_tigre("Rajah")
-zoo1.add_tigre("Shere Khan")
-zoo1.print_all_info()
+
+class Animals(Zoo):
+    def __init__(self, name, edad, salud, felicidad):
+        self.name = name
+        self.edad =edad
+        self.nivel_salud=salud
+        self.nivel_felicidad=felicidad
+
+    def display_info(self):
+        print('\nNombre:\t', self.name, '\nEdad:\t', self.edad, '\nNivel de salud:\t', self.nivel_salud, '\nNivel de felicidad:\t', self.nivel_felicidad)
+        return self
+
+    def alimentacion(self):
+        self.nivel_salud += 10
+        self.nivel_felicidad += 10
+
+class Leones(Animals):
+    def __init__(self, name, edad, liderazgo=100):
+        self.liderazgo = liderazgo
+        super().__init__(name, edad, 1000, 1000)
+
+    def display_info(self):
+        print('\nNombre:\t', self.name, '\nEdad:\t', self.edad, '\nNivel de salud:\t', self.nivel_salud, '\nNivel de felicidad:\t', self.nivel_felicidad, '\nNivel de liderazgo:\t', self.liderazgo)
+        return self
+
+    def alimentacion(self):
+        self.nivel_salud += 200
+        self.nivel_felicidad += 1000
+
+class Tigres(Animals):
+    def __init__(self, name, edad):
+        super().__init__(name, edad, 1000, 600)
+
+    def alimentacion(self):
+        self.nivel_salud += 500
+        self.nivel_felicidad += 100
+
+class Osos(Animals):
+    def __init__(self, name, edad):
+        super().__init__(name, edad, 800, 1000)
+
+
+
+zoologico=Zoo('Zoologico Nacional')
+zoologico.add_animal('Simba',30,'leon').alimentacion()
+zoologico.add_animal('Nala',20, 'tigre')
+zoologico.add_animal('Baloo',10,'oso')
+zoologico.print_all_info()
