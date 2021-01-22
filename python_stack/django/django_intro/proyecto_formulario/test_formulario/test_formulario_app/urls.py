cff706c897a933d535f6909e7d1efb1c7da5d17e
Catalina <CATALINA.ACUNAR@correoaiep.cl>
2021-01-21 22:51:24 -0300

    tarea random word

--- python_stack/django/django_intro/proyecto_formulario/test_formulario/test_formulario_app/urls.py
+++ python_stack/django/django_intro/proyecto_formulario/test_formulario/test_formulario_app/urls.py
@@ -1,5 +1,14 @@
+#from django.urls import path
+#from . import views
+#urlpatterns = [
+    #path('', views.index),
+#]
+
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index),
+    path('random', views.random)
+
 ]
--- python_stack/django/django_intro/proyecto_formulario/test_formulario/test_formulario_app/views.py
+++ python_stack/django/django_intro/proyecto_formulario/test_formulario/test_formulario_app/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render, HttpResponse
-def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+#from django.shortcuts import render, HttpResponse
+#def index(request):
+    #return HttpResponse("this is the equivalent of @app.route('/')!")
 
 #para trabajar con paginas web
 #from django.shortcuts import render	# notice the import!
 #def index(request):
     #return render(request, "index.html")
 
+from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
+
+
+from django.shortcuts import render
+from django.utils.crypto import get_random_string
+
+
+def function_1(request):
+    request.session['name'] = request.POST['name']
+    request.session['counter'] = 50
+
+def index(request):
+    request.session['counter'] = 0
+    return render(request, 'index.html')
+
+def random(request):
+    request.session['counter'] += 1
+    context = {
+        "word": get_random_string(length=14)
+    }
+    return render(request, 'index.html', context)
 # Create your views here.
