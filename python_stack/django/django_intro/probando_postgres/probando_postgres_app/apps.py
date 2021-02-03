99a7ebc1fd4bfbd7e55c2f978dc4e6c7b8acd17f
Catalina <CATALINA.ACUNAR@correoaiep.cl>
2021-02-02 22:20:50 -0300

    orm

---
+++ python_stack/django/django_intro/probando_postgres/probando_postgres_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProbandoPostgresAppConfig(AppConfig):
+    name = 'probando_postgres_app'
---
+++ python_stack/django/django_orm/djangoPy3Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\catalina\desktop\talento_digital\python-2020\python_stack\django\django_orm\djangopy3env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
